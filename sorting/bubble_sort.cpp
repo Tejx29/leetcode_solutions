c846d4547848ef4e1b379001239a60c71b79d844
tejas <tejasrp2905@gmail.com>
2026-07-23 00:42:28 +0530

    Bubble Sort

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -19,28 +19,6 @@ int main () {
         cin >> arr[i];
 
     }
-    
-    // int maxi;
-
-    // for(int i = 0; i < n - 1; i++) {
-
-    //     maxi = i;
-
-    //     for(int j = i + 1; j < n; j++) {
-
-    //         if(arr[j] > arr[maxi]) {
-
-    //             maxi = j;
-    //         }
-            
-    //         i += 1;
-
-    //     }
-
-    //     int temp = arr[maxi];
-    //     arr[maxi] = arr[i];
-    //     arr[i] = temp;
-    // }
 
     for(int i = n - 1; i > 0; i--) {                        // Total iterations
 
