3885c509e0c33a431a77e896ef7b5807534d6dde
tejas <tejasrp2905@gmail.com>
2026-07-23 17:44:11 +0530

    insertion_sort

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: " << endl;
+
+    for(int i = 0; i < n; i++) {
+
+        cin >> arr[i];
+
+    }
+
+    for(int i = 0; i < n; i++) {
+
+        int j = i;
+
+        cout << j << " " << endl;                           // For visualization
+
+        while(j > 0 && arr[j-1] > arr[j]) {
+
+            swap(arr[j-1], arr[j]);
+
+            cout << "Before: " << j << " ";                 // For visualization
+            
+            j--;
+
+            cout << "After: " << j << endl;                 // For visualization
+        }
+
+    }
+
+    cout << "Insertion Sort: ";
+
+    for(int i = 0; i < n; i++) {
+
+        cout << arr[i] << " ";
+
+    }
+
+    return 0;
+}
