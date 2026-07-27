c8157afc5d6509f7192d7394bb6d34257529c29d
tejas <tejasrp2905@gmail.com>
2026-07-28 01:06:15 +0530

    left_rotate_array

---
+++ arrays/left_rotate_array.cpp
@@ -0,0 +1,50 @@
+    #include <iostream>
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    void left_rotate (vector<int> &v1, int n) {
+        
+        int temp = v1[0];
+ 
+        for(int j = 1; j < n; j++) {
+
+            v1[j - 1] = v1[j];
+
+        }
+
+        v1[n - 1] = temp;
+
+        return;
+
+    }
+        
+    int main () {
+
+        int n;
+        
+        cout << "Enter size of array: ";
+        cin >> n;
+
+        vector <int> v1(n);
+
+        cout << "Enter array elements: " << endl;
+
+        for(int i = 0; i < n; i++) {
+
+            cin >> v1[i];
+
+        }
+
+
+        left_rotate (v1, n);
+
+        for(int i = 0; i < n; i++) {
+
+            cout << v1[i];
+
+        }
+
+        return 0;
+
+    }
