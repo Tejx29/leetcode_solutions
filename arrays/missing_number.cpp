271e0491a93fbc92514c32bd3bc87807aa6da390
tejas <tejasrp2905@gmail.com>
2026-08-02 22:31:26 +0530

    missing_number

--- arrays/missing_number.cpp
+++ arrays/missing_number.cpp
@@ -3,47 +3,39 @@
 
 using namespace std;
 
-int missing_num (int n, int hash[]) {
+int missing_num (vector<int> &arr, int N) {
+    
+    int xor1 = 0;
+    int xor2 = 0;
 
-    for(int i = 1; i <= n; i++) {
+    int n = N - 1;
 
-        if(hash[i] == 0) {
+    for(int i = 0; i < n; i++) {
 
-            return i;
-
-        }
+        xor2 = xor2 ^ arr[i];
+        xor1 = xor1 ^ (i + 1);
 
     }
 
-    return -1;
+    xor1 = xor1 ^ N;
 
+    return xor1 ^ xor2;
 }
 
 int main() {
 
-    int n;
+    int N;
 
-    cout << "Enter size of array: ";
-    cin >> n;
-    
-    vector<int> v(n);
-    
-    int hash[n + 1] = {0};
+    cout << "Enter N (largest): ";
+    cin >> N;
 
-    for(int i = 0; i < n; i++) {
+    vector<int> arr(N - 1);
 
-        cin >> v[i];
+    for(int i = 0; i < N - 1; i++) {
+
+        cin >> arr[i];
 
     }
 
-    for(int i = 0; i < n; i++) {
-
-        hash[v[i]] = 1;                          // or hash[v[i]]++;  
-
-    }
-
-    cout << missing_num (n, hash);
-
-    return 0;
-
+    cout << "Missing: " << missing_num (arr, N);
 }
