a0d717486996383172cff6dfe24a2484eada3c8b
tejas <tejasrp2905@gmail.com>
2026-08-04 23:31:42 +0530

    max_consec_1s

--- arrays/max_consec_1s.cpp
+++ arrays/max_consec_1s.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int max_consec_1s (vector<int> &v1, int n, int count, int maxi) {
+int max_consecutive_1s (vector<int> &v1, int n, int count, int maxi) {
 
     for(int i = 0; i < n; i++) {    
        
@@ -48,7 +48,7 @@ int main() {
     int count = 0;
     int maxi = 0;
 
-    cout << max_consec_1s (v1, n, count, maxi);    
+    cout << max_consecutive_1s (v1, n, count, maxi);    
 
     return 0;
 
