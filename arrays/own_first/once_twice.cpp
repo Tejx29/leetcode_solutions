224e214cbfdcf95ef12d155a3489c6a7f1265fab
tejas <tejasrp2905@gmail.com>
2026-08-06 00:49:45 +0530

    once_twice

--- arrays/own_first/once_twice.cpp
+++ arrays/own_first/once_twice.cpp
@@ -3,10 +3,49 @@
 
 using namespace std;
 
+int once_twice (vector<int> arr, int hash[]) {
+
+    int n = arr.size();
+
+    for(int i = 0; i < n; i++) {
+
+        if(hash[i] == 1) {
+
+            return i;
+
+        }
+
+    }
+
+} 
+
 int main() {
 
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
     
-    
+    int hash[20] = {0};
+
+    cout << "Enter array elements: " << endl;
+
+    for(int i = 0; i < n; i++) {
+
+        cin >> arr[i];
+
+    }
+
+    for(int i = 0; i < n; i++) {
+
+        hash[arr[i]]++;
+
+    }
+
+    cout << once_twice (arr, hash);
+
     return 0;
 
 }
