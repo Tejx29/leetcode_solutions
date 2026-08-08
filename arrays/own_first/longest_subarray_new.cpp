f054db80fad6b92a251a0c71e2f1b23cac2144e7
Tejx29 <tejasrp2905@gmail.com>
2026-08-08 22:48:24 +0530

    longest_subarray_new

--- arrays/own_first/longest_subarray_new.cpp
+++ arrays/own_first/longest_subarray_new.cpp
@@ -6,29 +6,27 @@ using namespace std;
 
 int longest_subarray_k_max (vector<int> &arr, int n, int k) {
 
+    int sum = 0;#include<iostream>
+#include<math.h>
+#include<algorithm>
+
+using namespace std;
+
+int longest_subarray_k_max (vector<int> &arr, int n, int k) {
+
     int sum = 0;
     int len = 0;
     int step = 0;
     
     for(int i = 0; i < n; i++) {
     
-        cout << "i: " << i << endl;
-    
         for(int j = step; j < n; j++) {
-    
-            cout << "j: " << j << endl;
             
             if(sum > k) {
-            
-                cout << "sum greater 1: " << sum << endl;
                 
                 sum -= arr[i];
                 
-                cout << "sum greater 1 after: " << sum << endl;
-                
                 step = j;
-                
-                cout << "step: " << step << endl;
                 
                 break;
             
@@ -36,28 +34,24 @@ int longest_subarray_k_max (vector<int> &arr, int n, int k) {
     
             sum += arr[j];
             
-            cout << "sum: " << sum << endl;
-            
             if(sum == k) {
             
-                len = j - i;
+                if(i == 0) {
                 
-                cout << "len: " << len << endl;
+                    len = (j - i) + 1;
+                
+                }
+                
+                len = j - i;
                 
             }
             
             if(sum > k) {
-            
-                cout << "sum greater 1: " << sum << endl;
                 
                 sum -= arr[i];
                 
-                cout << "sum greater 1 after: " << sum << endl;
-                
                 step = j;
                 
-                cout << "step: " << step << endl;
-            
             }
         
         }
