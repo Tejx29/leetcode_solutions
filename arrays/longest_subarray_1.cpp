966747626d8ee8cd6a6f4daf4f700fc527129af1
Tejx29 <tejasrp2905@gmail.com>
2026-08-09 21:16:32 +0530

    longest_subarray_1

---
+++ arrays/longest_subarray_1.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+
+using namespace std;
+
+int longest_subarray_k_max (vector<int> &arr, int n, int k) {
+
+    int sum = arr[0];
+    int len = 0;
+    
+    int left = 0;
+    int right = 0;
+    
+    while(right < n) {
+    
+        while(left <= right && sum > k) {
+        
+            sum -= arr[left];
+            left++;
+        
+        }
+    
+        if(sum == k) {
+            
+            len = max(len, right - left + 1);
+        
+        }
+    
+        right++;
+        if(right < n) {
+        
+            sum += arr[right];
+    
+        }
+    }
+    
+    return len;
+}
+
+int main () {
+
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter array elements: " << endl;
+
+    for(int i = 0; i < n; i++) {
+
+        cin >> arr[i];
+
+    }
+
+    int k;
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    cout << longest_subarray_k_max (arr, n, k);
+    
+    return 0;
+
+}
