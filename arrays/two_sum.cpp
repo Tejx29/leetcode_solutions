4033b152d6726dea3513993ca03516135e7bb686
Tejx29 <tejasrp2905@gmail.com>
2026-08-10 11:16:00 +0530

    two_sum

---
+++ arrays/two_sum.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+
+using namespace std;
+
+int two_sum (vector<int> &arr, int n, int target) {
+
+    for(int i = 0; i < n - 1; i++) {
+    
+        for(int j = 1; j < n; j++) {
+    
+            if(arr[i] + arr[j] == target) {
+            
+                cout << i << " " << j << endl;
+                
+            }
+    
+        }
+    
+    }
+
+    return 0;
+}
+
+int main () {
+
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter array elements: " << endl;
+
+    for(int i = 0; i < n; i++) {
+
+        cin >> arr[i];
+
+    }
+
+    int target;
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    cout << two_sum (arr, n, target);
+    
+    return 0;
+
+}
