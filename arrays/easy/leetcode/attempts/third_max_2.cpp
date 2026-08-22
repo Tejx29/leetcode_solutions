bd10d4c3989b6bf3c09554d17ecd90594306c2ff
tejas <tejasrp2905@gmail.com>
2026-08-22 23:38:39 +0530

    third_max_2

--- arrays/easy/leetcode/attempts/third_max_2.cpp
+++ arrays/easy/leetcode/attempts/third_max_2.cpp
@@ -5,10 +5,93 @@ using namespace std;
 
 int thirdMax(vector<int>& nums) {
 
-    auto max_val = max_element(nums.begin(), nums.end());
+    auto max_val = *max_element(nums.begin(), nums.end());
 
+    int first = max_val;
+    int second = 0;
+    int second_exists = INT_MIN;
+
+    int third = 0;
+    int third_exists = INT_MIN;
+
+    for(int i = 0; i < nums.size(); i++) {
+
+        cout << endl;
+
+        cout << "i: " << i << endl;
+
+        if(nums[i] == first || nums[i] == second || nums[i] == third) {
+
+            continue;
+
+        }
+
+        if(second == 0) {
+
+            second = nums[i]; 
+            second_exists++;
+
+        }
+
+        else if (third == 0 && nums[i] >= second) {
+
+            third = second;
+            second = nums[i];
+
+            third_exists++;
+        }
+
+        else if(nums[i] >= second) {
+
+            third = second;
+            second = nums[i];
+
+            second_exists++;
+
+        }
+        
+        else if(nums[i] >= third) {
+
+            third = nums[i];
+
+            third_exists++;
+
+        }
+
+        else if(nums[i] <= third) {
+
+            third = nums[i];
+            third_exists++;
+
+        }
+
+        
+        else if(third == 0) {
+
+            third = nums[i];
+            
+            third_exists++;
+
+        }
+        
+        cout << first << " " << second << " " << third << endl;
+
+    }
+
+    // cout << "Final: " << first << " " << second << " " << third << endl;
+
+    if (second_exists == INT_MIN || third_exists == INT_MIN) {
+
+        return first;
+
+    }
+
+    else {
+        
+        return third;
+
+    }
     
-
 }
 
 int main () {
