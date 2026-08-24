68df3a18b713e7a91773a7bfa129de38a025986d
tejas <tejasrp2905@gmail.com>
2026-08-24 22:48:22 +0530

    missing2

---
+++ arrays/easy/leetcode/attempts/missing2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void thirdMax(vector<int>& nums) {
+
+    int n = nums.size();
+    
+    set<int> s1;
+
+    for(int i = 0; i < n; i++) {
+
+        s1.emplace(nums[i]);
+
+    }
+
+    // for(int i = 1; i <= n; i++) {
+
+    //     cout << mp[i] << " ";
+
+    // }
+
+    for(int i = 1; i <= n; i++) {
+
+        if(s1.count(i)) {
+
+            continue;
+
+        }
+
+        cout << i << " ";
+    }
+}
+
+int main () {
+
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter array elements: " << endl;
+
+    for(int i = 0; i < n; i++) {
+
+        cin >> arr[i];
+
+    }
+
+    thirdMax (arr);
+    
+    return 0;
+
+}
