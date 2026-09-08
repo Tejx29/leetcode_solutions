0173daaf19142a44f20038701cece589e311b3d6
tejas <tejasrp2905@gmail.com>
2026-09-08 20:06:53 +0530

    update

--- arrays/easy/leetcode/attempts/valid_mountain_array_2.cpp
+++ arrays/easy/leetcode/attempts/valid_mountain_array_2.cpp
@@ -7,62 +7,7 @@ bool validMountainArray(vector<int>& arr) {
 
     int n = arr.size();
 
-    int up = 0;
-    int down = 0;
     
-    if(n < 3) {
-
-        return false;
-
-    }
-
-    auto max_Val = *max_element(arr.begin(), arr.end()); 
-    int max_idx = max_element(arr.begin(), arr.end()) - arr.begin();
-
-    for(int i = 0; i < n - 1; i++) {
-
-        if(arr[i] == arr[i + 1]) {
-
-            return false;
-
-        }
-        
-        else if(arr[i] < max_Val) {
-
-            if(down > 0) return false;
-            
-            up += 1;
-            continue;
-
-        }
-
-        else if (arr[i] < max_Val && i > max_idx) {
-
-            if(up == 0) return false; 
-            
-            down += 1;
-            continue;
-
-        }
-
-        else {
-
-            if(up == 0) return false; 
-
-            down += 1;
-            continue;
-
-        }
-
-    }
-
-    if(up == 0 || down == 0) {
-
-        return false;
-
-    }
-
-    return true;
 }
 
 int main () {
