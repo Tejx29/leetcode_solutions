bf10eef253bf4e1a12b3800bac68d17053ffa408
tejas <tejasrp2905@gmail.com>
2026-09-19 22:30:56 +0530

    we back

--- arrays/easy/leetcode/attempts/valid_mountain_array_2.cpp
+++ arrays/easy/leetcode/attempts/valid_mountain_array_2.cpp
@@ -7,7 +7,62 @@ bool validMountainArray(vector<int>& arr) {
 
     int n = arr.size();
 
+    int max = 0;
+
+    if(n < 3) {
+
+        return false;
+
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+
+        cout << "i: " << i << endl;
+        cout << "max: " << max << endl;
+
+        if(arr[i + 1] > arr[i]) {
+
+            if(max > 0) {
+
+                return false;
+
+            }
+
+            continue;
+
+        }
+
+        else if(arr[i + 1] < arr[i]) {
+
+            if(i == 0) return false;
+
+            max = arr[i];
+
+            continue;
+
+        }
+
+        else {
+
+            return false;
+
+        }
+
+    }
+
+    cout << "out" << endl;
+
+    if(max == 0) {
+
+        return false;
+
+    }
+
+    else {
+
+        return true;
     
+    }
 }
 
 int main () {
